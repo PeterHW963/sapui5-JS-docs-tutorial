bab60c3aa9eb33d266c5f0a4aa49af545919fee3
PeterHW963 <peterhadiwijaya963@gmail.com>
2025-11-07 17:37:13 +0800

    Descriptor for Applications v1

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,18 +2,13 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], (UIComponent, JSONModel, ResourceModel) => {
+], (UIComponent, JSONModel) => {
     "use strict";
     return UIComponent.extend("ui5.walkthrough.Component", {
         // metadata section
         metadata: {
-            "interfaces": ["sap.ui.core.IAsyncContentCreation"],
-            "rootView": {
-                "viewName": "ui5.walkthrough.view.App",
-                "type": "XML",
-                "id": "app"
-            }
+            interfaces: ["sap.ui.core.IAsyncContentCreation"],
+            manifest: "json"
         },
 
         // init section: called when component is initialized
@@ -29,12 +24,6 @@ sap.ui.define([
             };
             const oModel = new JSONModel(oData);
             this.setModel(oModel);
-
-            // set i18n (internationalization) model on view
-            const i18nModel = new ResourceModel({
-                bundleName: "ui5.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
         }
     })
 })
