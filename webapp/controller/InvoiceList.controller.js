18e00b662c00a1804668959da490257212e57ad9
PeterHW963 <peterhadiwijaya963@gmail.com>
2025-11-09 08:15:05 +0800

    Data types v2

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,15 +1,14 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
-  function (Controller, JSONModel) {
+sap.ui.define([
+    "sap/ui/core/mvc/Controller", 
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
-
     return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
-      onInit() {
-        const oViewModel = new JSONModel({
-          currency: "EUR",
-        });
-        this.getView().setModel(oViewModel, "view");
-      },
+        onInit() {
+            const oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view");
+        }
     });
-  }
-);
+});
