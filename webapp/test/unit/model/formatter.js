43e600fc7c3d36e3a429ea38ad1da7709177b3d8
PeterHW963 <peterhadiwijaya963@gmail.com>
2025-11-10 10:44:36 +0800

    Unit Test with Qunit v1

---
+++ webapp/test/unit/model/formatter.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+    "ui5/walkthrough/model/formatter",
+    "sap/ui/model/resource/ResourceModel"
+], (formatter, ResourceModel) => {
+    "use strict";
+
+    QUnit.module("Formatting functions", {});
+
+    QUnit.test("Should return the translated texts", (assert) => {
+        const oResourceModel = new ResourceModel({
+            bundleUrl: sap.ui.require.toUrl("ui5/walkthrough/i18n/i18n.properties"),
+            supportedLocales: [""],
+            fallbackLocale: ""
+        });
+
+        const oControllerMock = {
+            getOwnerComponent() {
+                return {
+                    getModel() {
+                        return oResourceModel;
+                    }
+                }
+            }
+        };
+
+        const fnIsolatedFormatter = formatter.statusText.bind(oControllerMock);
+
+        // Assert
+        assert.strictEqual(fnIsolatedFormatter("A"), "New", "The long text for status A is incorrect");
+        assert.strictEqual(fnIsolatedFormatter("B"), "In Progress", "The long text for status B is incorrect");
+        assert.strictEqual(fnIsolatedFormatter("C"), "Done", "The long text for status C is incorrect");
+        assert.strictEqual(fnIsolatedFormatter("MockStatus"), "MockStatus", "The long text for Status MockStatus is incorrect");
+    });
+});
